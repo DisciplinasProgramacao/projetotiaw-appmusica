ec873b694ce502c9bab933b6d67efca9e9a7ee8c
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2022-11-08 22:44:37 -0300

    Login Navegacão
    Navegação do login para o cadastro criada

---
+++ src/modules/Login/index.js
@@ -0,0 +1,32 @@
+const USER_DATA = "LOCALSTORAGE_USER_DATA"
+
+// Name and Password from the register-form
+var name = document.getElementById('name');
+var password = document.getElementById('pw');
+
+
+
+// storing input from register-form
+function store() {
+    localStorage.setItem('name', name.value);
+    localStorage.setItem('password', password.value);
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+
+    // stored data from the register-form
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('password');
+
+    // entered data from the login-form
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    // check if stored data from register-form is equal to data from login form
+    if(userName.value !== storedName || userPw.value !== storedPw) {
+        alert('ERROR');
+    }else {
+        alert('Login efetuado com sucesso.');
+    }
+}
