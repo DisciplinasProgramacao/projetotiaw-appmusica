34b29663743b60b5d853252da1c2d39a9ff7dd88
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2022-11-14 09:04:13 -0300

    Add files via upload
    Botão "acessar" redirecionando para a página "HOME" e "criar conta" redirecionando para "SIGNUP".

--- src/modules/Login/index.js
+++ src/modules/Login/index.js
@@ -1,32 +1,13 @@
-const USER_DATA = "LOCALSTORAGE_USER_DATA"
-
-// Name and Password from the register-form
-var name = document.getElementById('name');
-var password = document.getElementById('pw');
-
-
-
-// storing input from register-form
-function store() {
-    localStorage.setItem('name', name.value);
-    localStorage.setItem('password', password.value);
+function logar(){
+var name = document.getElementById("name");    
+var password = document.getElementById("password");    
+if(name.value == "admin" && password.value == "admin"){
+    localStorage.setItem("acessar", true);
+    alert("Acesso concedido");
+     window.location.href= "../Profile/index.html";
+  }else{
+    alert("Usuário ou senha incorreto!!");
 }
+localStorage.setItem(USER_DATA, JSON.stringify(data))
 
-// check if stored data from register-form is equal to entered data in the   login-form
-function check() {
-
-    // stored data from the register-form
-    var storedName = localStorage.getItem('name');
-    var storedPw = localStorage.getItem('password');
-
-    // entered data from the login-form
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPw');
-
-    // check if stored data from register-form is equal to data from login form
-    if(userName.value !== storedName || userPw.value !== storedPw) {
-        alert('ERROR');
-    }else {
-        alert('Login efetuado com sucesso.');
-    }
 }
